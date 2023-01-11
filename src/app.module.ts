804dfff0c803ac4e66960f0cc51723ebdb89eabe
RobsonWairokpam <robson23174@gmail.com>
2023-01-11 14:06:15 +0530

    newly add only in nestJs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, DataModule],
+  imports: [PrismaModule, DataModule, ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -7,12 +7,101 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+// import axios from 'axios';
+import fetch from 'node-fetch';
+import { PrismaService } from '../prisma/prisma.service';
 import { RecordDataInput, ServerDataInput } from './data.model';
 import { DataService } from './data.service';
 
 @Controller('/data')
 export class DataController {
-  constructor(private readonly service: DataService) {}
+  constructor(
+    private readonly service: DataService,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  // @Get('/d')
+  @Cron('* */5 * * * *')
+  async datas() {
+    const data = await this.prisma.server.findMany();
+
+    const abc = Promise.resolve(data.map((e) => e.url));
+    Promise.all([abc]).then((value) => {
+      console.log('RRRR', value[0]);
+
+      for (const address of value[0]) {
+        const url = new URL(address);
+        const newAddr = `https://${url.host}/`;
+        console.log('ABAVA>>>', url.host);
+
+        fetch(newAddr, {
+          method: 'GET',
+        })
+          .then((res) => {
+            if (res.status / 100 === 2) {
+              return this.prisma.record.create({
+                data: {
+                  status: 'Up',
+                  server: {
+                    connect: {
+                      url: newAddr,
+                    },
+                  },
+                },
+              });
+            }
+          })
+          .catch((e: any) => {
+            if (e) {
+              return this.prisma.record.create({
+                data: {
+                  status: 'Down',
+                  server: {
+                    connect: {
+                      url: newAddr,
+                    },
+                  },
+                },
+              });
+            }
+          });
+        // axios
+        //   .get(newAddr)
+        //   .then((res) => {
+        //     // console.log({ [address]: res.status });
+        //     if (res.status / 100 === 2) {
+        //       return this.prisma.record.create({
+        //         data: {
+        //           status: 'Up',
+        //           server: {
+        //             connect: {
+        //               url: newAddr,
+        //             },
+        //           },
+        //         },
+        //       });
+        //       // console.log('SUUCES', newAddr);
+        //     }
+        //   })
+        // .catch((err) => {
+        //   if (err) {
+        //     return this.prisma.record.create({
+        //       data: {
+        //         status: 'Down',
+        //         server: {
+        //           connect: {
+        //             url: newAddr,
+        //           },
+        //         },
+        //       },
+        //     });
+        //   }
+        //     console.error({ [address]: err.code });
+        //   });
+      }
+    });
+  }
 
   @Post('/status/create')
   createRecords(
